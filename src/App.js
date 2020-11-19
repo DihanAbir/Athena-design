4d95dced9045ca22016d31e78edeb9ffd038d302
DihanAbir <nahid.muradabir@gmail.com>
2020-11-19 11:00:25 +0600

    athena boilar plate ready

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Landingpage from './Component/Pages/LandingPage/Landingpage';
+
+function App() {
+  return (
+    <div className="App">
+      <Landingpage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Pages/LandingPage/Landingpage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Landingpage = () => {
+    return (
+        <div>
+            <h1 className="ml-5">Lnading page</h1>
+            <Header/>
+        </div>
+    );
+};
+
+export default Landingpage;
